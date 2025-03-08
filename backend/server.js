642c30d79ca7bee2b0a70e49fc26a2db440e7c2c
Abhinav Sharma <abhisharma19091999@gmail.com>
2025-03-09 01:25:07 +0530

    Deploy completed

--- backend/server.js
+++ backend/server.js
@@ -15,13 +15,7 @@ const app = express();
 
 // Middleware
 app.use(express.json());
-app.use(cors(
-  {
-      origin: ["https://insightify-frontend.vercel.app"],
-      methods: ["POST", "GET"],
-      credentials: true
-  }
-));
+app.use(cors());
 
 app.use(express.urlencoded({ extended: true }));
 
